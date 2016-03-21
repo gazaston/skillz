a351afda0b3a2a7fb5d8cb9bc68ed5c2280a5171
Gaz Aston <spiritbased@gmail.com>
2016-03-21 18:34:56 +0000

    add People to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'users#index'
   # resources :slack_responses, only: :create
   resources :slack_responses, only: [:index, :new, :create]
+  resources :people, only: [:index, :new, :create]
 
 end
